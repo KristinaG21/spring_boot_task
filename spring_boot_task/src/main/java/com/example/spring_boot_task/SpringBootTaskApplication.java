381e2aa65d8149b8463011d2d95970815db5ae53
KristinaG21 <kristina.gejenko@gmail.com>
2020-10-14 14:40:01 +0300

    1.Create a Spring Boot service (use Java and maven).

--- spring_boot_task/src/main/java/com/example/spring_boot_task/SpringBootTaskApplication.java
+++ spring_boot_task/src/main/java/com/example/spring_boot_task/SpringBootTaskApplication.java
@@ -1,35 +1,19 @@
 package com.example.spring_boot_task;
 
-import com.example.spring_boot_task.model.User;
-import com.example.spring_boot_task.repository.UserRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class SpringBootTaskApplication {
 
-	private static final Logger log = LoggerFactory.getLogger(SpringBootTaskApplication.class);
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootTaskApplication.class, args);
 	}
 
-	@Bean
-	public CommandLineRunner runner(UserRepository repository) {
-		return (args) -> {
-			repository.save(new User("Jack", "Bauer",35));
-			repository.save(new User("Chloe", "O'Brian",46));
-			repository.save(new User("Kim", "Bauer",30));
 
-
-			repository.findAll().forEach(user -> {
-				log.info("User: {}", user);
-			});
-		};
-	}
 
 }
