dcd8784c53dff749066f363426318b52638d0218
KristinaG21 <kristina.gejenko@gmail.com>
2020-10-22 14:12:56 +0300

    Task 3. Cache

--- spring_boot_task/src/main/java/com/example/spring_boot_task/service/UserService.java
+++ spring_boot_task/src/main/java/com/example/spring_boot_task/service/UserService.java
@@ -27,21 +27,23 @@ public class UserService {
 
 
     @Scheduled(cron = "${cron.expression}")
-    @Cacheable(value = "users")
+//    @Cacheable(value = "users")
     public List<User> getAllUsers() {
         System.out.println("getAll service call in " + new Date().toString());
+        simulateSlowService();
         return userRepository.findAll();
 
     }
 
     @Cacheable(value = "users", key="#id")
     public User getById(int id) {
-       log.info("fetch book from db");
+        log.info("getting user by id: {}", id);
         return userRepository.getById(id);
     }
 
     @Cacheable(value = "users", key="#name")
     public Optional<User> getByName(String name) {
+        log.info("getting user by name: {}", name);
        return userRepository.findByFirstName(name);
 
 
@@ -49,11 +51,22 @@ public class UserService {
 
     @Cacheable(value = "users", key="#age")
     public Optional<User> getByAge(int age) {
+        log.info("getting user by age: {}", age);
         return userRepository.findByAgeGreaterThan(age);
     }
 
     public User saveUser(User user) {
+        log.info("save user: {}", user);
         return userRepository.save(user);
+    }
+
+    private void simulateSlowService() {
+        try {
+
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new IllegalStateException(e);
+        }
     }
 
 
